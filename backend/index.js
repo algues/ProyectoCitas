5e2e8e456d6c594b977c7f348094de59763bcf52
Alfredo <alf10guerra68@hotmail.com>
2024-11-21 20:56:29 -0500

    Adición archivo index.js del Backend

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const app = mongoose();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/Documental')   
+    .then(db => console.log('conexion exitosa'))
+    .catch(err => console.log('error: ', err))
+
+app.get("/", (req,res) => {
+    res.json("Hello this is the Backend")
+    });
+    
+
+app.listen(8800, () =>{
+    console.log("Connected to Backend");
+});
