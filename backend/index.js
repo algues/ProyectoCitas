7ff8b54c23552c78254772a1a4d20f7693505667
Alfredo <alf10guerra68@hotmail.com>
2025-02-19 17:50:48 -0500

    Archivo index.js modificado3

--- backend/index.js
+++ backend/index.js
@@ -132,6 +132,82 @@ app.put('/pacientes/:_id', async (req,res)=>{
     res.json(result)
 });
 
+app.put('/medicos/:_id', async (req,res) =>{
+    const medicoId = req.params._id;
+    const result = await medicoModel.findByIdAndUpdate({_id:medicoId}, {
+        identificacion: rq.body.identificacion,
+        apellidos: req.body.apellidos,
+        nombres: req.body.nombres,
+        especialidad: req.body.especialidad
+    })
+    res.json(result)
+});
+
+app.put('/consultorios/:_id', async (req,res) =>{
+    const consultorioId = req.params._id;
+    const result = await consultorioModel.findByIdAndUpdate({_id:consultorioId}, {
+        nombre: req.body.nombre
+    })
+    res.json(result)
+});
+
+app.put('/citas/:_id', async (req,res) =>{
+    const citasId = req.params._id;
+    const result = await citasModel.findByIdAndUpdate({_id:citasId}, {
+        fecha: req.body.fecha,
+        hora: req.body.hora,
+        paciente: req.body.paciente,
+        medico: req.body.paciente,
+        consultorio: req.body.consultorio,
+        estado: req.body.estado,
+        observaciones: req.body.observaciones  
+    })
+    res.json(result)
+});
+
+app.put('tratamientos/:_id', async (req,res) =>{
+    const tratamientosId = req.params._id;
+    const result = await tratamientosModel.findByIdAndUpdate({_id:tratamientosId}, {
+        fechAsignada: req.body.fechaAsignada,
+        descripcion: req.body.descripcion,
+        fechaInicio: req.body.fechaInicio,
+        fechaFin: req.body.fechaFin,
+        observaciones: req.body.observaciones,
+        paciente: req.body.paciente
+    })
+    res.json(result)
+});
+
+app.delete('/pacientes/:_id', async (req,rs) =>{
+    const pacienteId = req.params._id;
+    const result = await pacienteModel.deleteOne({_id:pacienteId})
+    res.json(result)
+});
+
+app.delete('/medicos/:_id', async (req,res) =>{
+    const medicoId = req.params._id;
+    const result = await medicoModel.deleteOne({_id:medicoId})
+    res.json(result)
+});
+
+app.delete('/consultorios/:_id', async (req,res) =>{
+    const consultorioId = req.params._id;
+    const result = await consultorioModel.deleteOne({_id:consultorioId})
+    res.json(result)
+});
+
+app.delete('/citas/:_id', async (req,res) =>{
+    const citasId = req.params._id;
+    const result = await citasModel.deleteOne({_id:citasId})
+    res.json(result)
+});
+
+app.delete('/tratamientos/:_id', async (req,res) =>{
+    const tratamientosId = req.params._id;
+    const result = await tratamientosModel.deleteOne({_id:tratamientosId})
+    res.json(result)
+})
+
 app.listen(8800, () => {
     console.log('Connected to Backend')
 });
