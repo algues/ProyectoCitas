d68b241ff0714d1b2e6129e57c2f1d26fd2146c5
Alfredo <alf10guerra68@hotmail.com>
2025-03-18 20:24:06 -0500

    Archivo App.js del frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Router>
+           <Routes>
+               <Route path='/' element={<Login />}></Route>
+               <Route path='/home' element={<Home />}></Route>
+               <Route path='/pacientes' element={<Pacientes />}></Route>               
+               <Route path='/addPacientes' element={<AddPacientes />}></Route>
+               <Route path='/updatePacientes/:_id' element={<UpdatePacientes />}></Route>
+               <Route path='/medicos' element={<Medicos />}></Route>
+               <Route path='/addMedicos' element={<AddMedicos />}></Route>
+               <Route path='/updateMedicos/:_id' element={<UpdateMedicos />}></Route>
+               <Route path='/consultorios' element={<Consultorios />}></Route>
+               <Route path='/addConsultorios' element={<AddConsultorios />}></Route>
+               <Route path='/updateConsultorios/:_id' element={<UpdateConsultorios />}></Route>
+               <Route path='/citas' element={<Citas />}></Route>
+               <Route path='/addCitas' element={<AddCitas />}></Route>
+               <Route path='/updateCitas/:_id' element={<UpdateCitas />}></Route>
+               <Route path='/tratamientos' element={<Tratamientos />}></Route>
+               <Route path='/addTratamiento' element={<AddTratamientos />}></Route>
+               <Route path='/updateTratamiento/:_id' element={<UpdateTratamintos />}></Route>
+           </Routes>
+       </Router>          
+      </header>
+    </div>
+  );
+}
+
+export default App;
