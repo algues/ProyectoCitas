84fd2b7541294a7176ff9d02586a67f5079351d7
Alfredo <alf10guerra68@hotmail.com>
2025-05-16 18:56:18 -0500

    Archivo home.jsx del frontend

---
+++ frontend/src/components/home.jsx
@@ -0,0 +1,19 @@
+import react  from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return(
+        <div>
+            <h3 className='titulo'>Centro Médico la María</h3>
+            <button className='btn btn-info'><Link to="/home">Home</Link></button>
+            <button className='btn btn-warning'><Link to="/pacientes">Pacientes</Link></button>
+            <button className='btn btn-info'><Link to="/medicos"></Link>Médicos</button>
+            <button className='btn btn-warning'><Link to="/consultorios">Consultorios</Link></button>
+            <button className='btn btn-info'><Link to="/citas">Citas</Link></button>
+            <button className='btn btn-warning'><Link to="/tratamientos">Tratamientos</Link></button>
+            <button className='btn btn-info'><Link to="/">Logout</Link></button>
+        </div>
+    )
+}
+
+export default Home;
