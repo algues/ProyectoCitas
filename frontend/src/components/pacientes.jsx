236f3cf7661c47ee78d8aacd61e7102b17f35cd4
Alfredo <alf10guerra68@hotmail.com>
2025-05-20 11:54:35 -0500

    Archivo pacientes.jsx del frontend

---
+++ frontend/src/components/pacientes.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Pacientes = () => {
+    const [pacientes, setPacientes] = useState([]);
+
+    useEffect(() => {
+        const fecthAllPacientes = async () =>{
+            try{
+                const res = await axios.get("http://localhost:8800/pacientes/");
+                setPacientes(rs.data);
+
+            }catch(err){
+                console.log(err)
+            }
+        }
+        fecthAllPacientes();
+        
+    }, []);
+
+    const handleDelete = async (_id) => {
+        try{
+           await axios.delete("http://localhost:8800/pacientes/"+ _id);
+           window.location.reload();
+        }catch(err){
+           console.log(err)
+        }
+    }
+
+    return(
+        <did>
+            <Table>
+                <thead>
+                   <tr>
+                     <td colSpan={7}><h3 className='titulo'>Administrar Pacientes</h3></td>
+                   </tr> 
+                   <tr className='table-info'>
+                     <th>Id</th>               
+                     <th>Identificacion</th> 
+                     <th>Apellidos</th>
+                     <th>Nombres</th>
+                     <th>Fecha Nacimiento</th>
+                     <th>Sexo</th>
+                     <th>Acciones</th>
+                   </tr>        
+                </thead>
+                <tbody>
+                     {pacientes.map((paciente) =>(
+                        <tr key={paciente._id}>
+                           <td>{paciente._id}</td>
+                           <td>{paciente.identificacion}</td>
+                           <td>{paciente.apellidos}</td>
+                           <td>{paciente.nombres}</td>
+                           <td>{paciente.fechaNacimiento}</td>
+                           <td>{paciente.sexo}</td>
+                           <button className='btn btn-danger' onClick={() => handleDelete(paciente._id)}>Delete</button>
+                           <button className='btn btn-info'><Link to={`/updatePacientes/${paciente._id}`}>Update</Link></button>
+                        </tr>
+                     ))}
+                </tbody>
+                <tr>
+                  <td colSpan={7}>
+                    <button className='btn btn-info'><Link to="/addPacientes">Add Paciente</Link></button>
+                    <button className='btn btn-warning'><Link to="/home">Home</Link></button>
+                  </td>
+                </tr>
+            </Table>
+        </did>
+    )
+}
+
+export default Pacientes;
