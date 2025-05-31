6c7d4257c0a93f1b4da927d4bed6681cbc5d0263
Alfredo <alf10guerra68@hotmail.com>
2025-05-30 19:12:58 -0500

    Archivo updateConsultorios.jsx del frontend

---
+++ frontend/src/components/consultorios/updateConsultorios.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link, useLocation, useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const UpdateConsultorios = () => {
+
+    const { _id } = useParams();
+
+    useEffect(() =>{
+        axios.get("http://localhost:8800/consultorios/"+_id)
+        .then(res => {
+            console.log(res)
+            setConsultorio(res.data[0])
+        })
+        .catch(err => console.log(err))
+    }, [])
+
+    const [consultorio, setConsultorio] = useState({
+        nombre: ""       
+    });
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const consultorioId = location.pathname.split("/")[2];
+
+    const handleChange = (e) =>{
+        setConsultorio(prev =>({...prev, [e.target.name]: e.target.value}));
+    };
+
+    const handleClick = async e =>{
+        e.preventDefault()
+        try {
+            await axios.put("http://localhost:8800/consultorios/"+ consultorioId, consultorio)
+            navigate('/consultorios')
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    return(
+        <div className="container">
+          <form className="form">
+             <h5 className="titulo">Modificar Consultorios</h5>
+             <label>Nombre:</label>
+             <input type="text" name="nombre" onChange={handleChange} value={consultorio.nombre}></input>
+             <button className="btn btn-warning" onClick={handleClick}>Update</button>
+             <button className="btn btn-info"><Link to="/medicos">Médicos</Link></button>
+          </form> 
+        </div>  
+    )
+}
+
+export default UpdateConsultorios;
