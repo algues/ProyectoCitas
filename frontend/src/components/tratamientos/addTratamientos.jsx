a1858b925844ba88d451947107ec30504a311407
Alfredo <alf10guerra68@hotmail.com>
2025-05-30 20:23:37 -0500

    Archivo addTratamientos.jsx del frontend

---
+++ frontend/src/components/tratamientos/addTratamientos.jsx
@@ -0,0 +1,64 @@
+import react, { useState } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+
+
+const AddTratamientos = () => {
+     const  [tratamientos, setTratamientos] = useState({
+             fechaAsignada: "",
+             descripcion: "",
+             fechaInicio: "",
+             fechaFin: "",
+             observaciones: "",
+             paciete: ""             
+         })
+     
+         const navigate = useNavigate();
+     
+         const handleChange = (e) =>{
+             setTratamientos(prev =>({...prev, [e.target.name]:e.taget.value}));
+         };
+     
+         const handleClick = async e =>{
+             e.preventDefault()
+             try{
+                 await axios.post("http://localhost:8000/tratamientos", tratamientos)
+                 .then(res =>{
+                     if(res.data == "Success"){
+                         window.alert("Trataminto registrado con éxito")
+                         navigate('/tratamientos')
+                     }else{
+                         window.alert("Registro fallido")
+                     }
+                 })
+             }catch(err){
+                 console.log(err)
+             }
+         }
+     
+         return(
+         <div>
+            <form>
+               <h5>Adición de Tratamientos</h5>
+               <label>Fecha Asignada:</label>
+               <input type='date' name="fechaAsignada" onChange={handleChange}></input>
+               <label>Descripcion:</label>
+               <textarea type='text' name='descripcion' onChange={handleChange} rows={3}></textarea>
+               <label>Fecha Inicio:</label>
+               <input type='date' name='fechaInicio' onChange={handleChange}></input>
+               <label>Fecha Fin:</label>
+               <input type='date' name='fechaFin' onChange={handleChange}></input>
+               <label>Observaciones:</label>
+               <textarea type='text' name='observaciones' onChange={handleChange} row="3"></textarea>
+               <label>Paciente:</label>
+               <input type='number' name='paciente' onChange={handleChange}></input>
+               <button className="btn btn-info" onClick={handleClick}>Registrar</button>
+               <button className="btn btn-warning"><Link to="/tratamientos">Tratamientos</Link></button>
+            </form>
+         </div>
+         )
+     
+
+}
+
+export default AddTratamientos;
