3b53b3d7521a8c27a7e92ac1a45c37596a5e4138
Alfredo <alf10guerra68@hotmail.com>
2025-06-04 14:51:34 -0500

    Archivo addConsultorios.jsx modificado del frontend

---
+++ frontend/src/components/consultorios/addConsultorios.jsx
@@ -0,0 +1,48 @@
+import react, { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
+const AddConsultorios = () =>{
+
+    const  [consultorio, setConsultorio] = useState({
+        nombre: ""
+    })
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) =>{
+        setConsultorio(prev =>({...prev, [e.target.name]:e.taget.value}));
+    };
+
+    const handleClick = async e =>{
+        e.preventDefault()
+        try{
+            await axios.post("http://localhost:8000/consultorios", consultorio)
+            .then(res =>{
+                if(res.data == "Success"){
+                    window.alert("Consultorio registrado con éxito")
+                    navigate('/consultorio')
+                }else{
+                    window.alert("Registro fallido")
+                }
+            })
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+    return(
+    <div className="container">
+       <form className="form">
+          <h5>Adición de Consultorios</h5>
+          <label>Nombre Consultorio:</label>
+          <input type='text' name="nombre" onChange={handleChange}></input>
+          <button className="btn btn-info" onClick={handleClick}>Registrar</button>
+          <button className="btn btn-warning"><Link to="/consultorios">Consultorios</Link></button>
+       </form>
+    </div>
+    )
+
+}
+
+export default AddConsultorios;
