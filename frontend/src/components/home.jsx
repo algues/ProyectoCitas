2154d336a8ce93029d55333346259f26ca903c2e
Alfredo <alf10guerra68@hotmail.com>
2025-07-24 20:17:45 -0500

    archivo home.jsx del frontend modificado

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -1,18 +1,33 @@
 import react  from 'react';
 import { Link } from 'react-router-dom';
+import { Dropdown } from 'react-bootstrap';
+
 
 const Home = () => {
     return(
-        <div>
-            <h3 className='titulo'>Centro Médico la María</h3>
-            <button className='btn btn-info'><Link to="/home">Home</Link></button>
+        <Dropdown>
+            <h3 className='titulo'>Centro Médico la María</h3>            
             <button className='btn btn-warning'><Link to="/pacientes">Pacientes</Link></button>
             <button className='btn btn-info'><Link to="/medicos">Medicos</Link></button>
             <button className='btn btn-warning'><Link to="/consultorios">Consultorios</Link></button>
             <button className='btn btn-info'><Link to="/citas">Citas</Link></button>
-            <button className='btn btn-warning'><Link to="/tratamientos">Tratamientos</Link></button>
-            <button className='btn btn-info'><Link to="/">Logout</Link></button>
-        </div>
+            <button className='btn btn-warning'><Link to="/tratamientos">Tratamientos</Link></button>               
+            
+               <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                  Reportes
+               </Dropdown.Toggle>
+
+               <Dropdown.Menu>
+                 <Dropdown.Item href="/reportePacientes">Reporte de Pacientes</Dropdown.Item>
+                 <Dropdown.Item href="/reporteMedicos">Reporte de Médicos</Dropdown.Item>
+                 <Dropdown.Item href="/reporteConsultorios">Reporte de Consultorios</Dropdown.Item>
+                 <Dropdown.Item href='/reporteCitas'>Reporte de Citas</Dropdown.Item>
+                 <Dropdown.Item href='/reporteTratamientos'>Reporte de Tratamientos</Dropdown.Item> 
+               </Dropdown.Menu>
+                                                   
+            <button className='btn btn-warning'><Link to="/">Logout</Link></button>
+        </Dropdown>    
+        
     )
 }
 
