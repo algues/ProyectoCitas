db1c7cf8c28d87616020771bb9094e20c7403866
algues <118970766+algues@users.noreply.github.com>
2025-07-26 12:01:56 -0500

    addMedicos.jsx modificado

--- frontend/src/components/medicos/addMedicos.jsx
+++ frontend/src/components/medicos/addMedicos.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useState } from 'react';
 import { Link, useNavigate} from 'react-router-dom';
 
-const AddPacientes = () => {
+const AddMedicos = () => {
 
     const [medico, setMedico] = useState({
         identificacion: "",
@@ -55,4 +55,4 @@ const AddPacientes = () => {
     )
 }
 
-export default AddPacientes;
+export default AddMedcos;
